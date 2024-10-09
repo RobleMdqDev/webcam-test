eaaa8614095ae99477477951b8ff131f9cd6f005
Alberto Robledo <robledo.mdp@gmail.com>
2024-10-09 15:46:05 -0300

    next.config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true, // Activar el minificador SWC para mejorar el rendimiento
+};
 
 export default nextConfig;
